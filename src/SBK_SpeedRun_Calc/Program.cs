688c4a205ea793128138cd37b64eaecb3eea5023
FilthensteinsMonster <filthenstein@gmail.com>
2025-02-24 23:23:53 -0600

    got launch working

--- src/SBK_SpeedRun_Calc/Program.cs
+++ src/SBK_SpeedRun_Calc/Program.cs
@@ -12,17 +12,19 @@ namespace SBK_SpeedRun_Calc
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
             string exePath = Path.GetDirectoryName(assembly.Location);
-            string dir = Path.Combine(exePath,"UserInput"); 
+            string dir = Path.Combine(exePath, "UserInput"); 
 
             string configFile = "KeyBind.txt";
             string dataSeed = "SeedData.txt";
 
             if(!Directory.Exists(dir)){
                 Console.WriteLine("Error, folder not found: " + dir);
+            } else {
+                Console.WriteLine("files found");
             }
             
             Console.WriteLine("Prog complete. press any key to exit");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
