1b5a49eb2bb263dab5dde3598e2f16ead03aea4c
FilthensteinsMonster <filthenstein@gmail.com>
2025-02-24 22:29:41 -0600

    getting basics set up, installing node.js, comment before install

---
+++ src/SBK_SpeedRun_Calc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SBK_SpeedRun_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string exePath = Path.GetDirectoryName(assembly.Location);
+            string dir = Path.Combine(exePath,"UserInput"); 
+
+            string configFile = "KeyBind.txt";
+            string dataSeed = "SeedData.txt";
+
+            if(!Directory.Exists(dir)){
+                Console.WriteLine("Error, folder not found: " + dir);
+            }
+            
+            Console.WriteLine("Prog complete. press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
